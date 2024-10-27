52a24e8a6a1cb9d38e602b300477a34c98fdf0c4
Steven Webb <xiaoancloud@outlook.com>
2024-10-27 18:23:20 +1100

    init push

---
+++ src/main/java/net/evegents/go/EveGEntsGoApplication.java
@@ -0,0 +1,13 @@
+package net.evegents.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveGEntsGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveGEntsGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/evegents/go/EveGEntsGoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.evegents.go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveGEntsGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
